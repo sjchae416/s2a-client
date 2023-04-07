100e128937ac0151948b489edf90f27ef384d503
Seungjun Chae <seungjun.chae@stonybrook.edu>
2023-04-07 01:20:51 -0400

    fixed App.js and index.js structure

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React from 'react';
 import {
-  BrowserRouter,
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
-import "./App.css";
-import Box from "@mui/material/Box";
-import { createBrowserHistory } from "history";
+	BrowserRouter,
+	// BrowserRouter as Router,
+	Route,
+	Routes,
+	Switch,
+} from 'react-router-dom';
+import './App.css';
+import Box from '@mui/material/Box';
+import { createBrowserHistory } from 'history';
 import {
-  Dashboard,
-  Login,
-  CreateApp,
-  ManageTable,
-  TableView,
-  Table
-} from "./components";
+	Dashboard,
+	Login,
+	CreateApp,
+	ManageTable,
+	TableView,
+	Table,
+} from './components';
 
 export const customHistory = createBrowserHistory();
 const App = () => {
-  return (
-    <Box>
-      <Router forceRefresh={true}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/create-table" element={<Table />} />
+	return (
+		<Box>
+			{/* <Router forceRefresh={true}> */}
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/create-table" element={<Table />} />
 
-          <Route path="/add-view" element={<CreateApp />} />
-          <Route path="/manage-table" element={<ManageTable />} />
-          <Route path="/table-view" element={<TableView />} />
-        </Routes>
-      </Router>
-    </Box>
-  );
+				<Route path="/add-view" element={<CreateApp />} />
+				<Route path="/manage-table" element={<ManageTable />} />
+				<Route path="/table-view" element={<TableView />} />
+			</Routes>
+			{/* </Router> */}
+		</Box>
+	);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-
-ReactDOM.render(
-	<React.StrictMode>  
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-  );
+root.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
+);
