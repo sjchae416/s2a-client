6cc7e4acf5de520972b8a5ecdb4c03f3d83750bd
Christina-Chau <christinachau5@gmail.com>
2023-04-11 11:58:33 -0400

    saved user input in config table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import role from "../testData/test-role-sheet.json";
 
 import ConfigTable from "./ConfigTable";
 
@@ -12,18 +10,26 @@ export default function Table({ tablelist, setTableList }) {
   const [columns, setColumns] = useState([]); // list of column names in the data
   const [tableDataArray, setTableDataArray] = useState([]);
 
+
+  const keys = Object.values(columns);
+  const dummyRef = ["test1", "test2"];
+  const [config, setConfig] = useState([]);
+
   const tableData = {
     name: name,
     url: url,
     sheetIndex: sheetIndex,
-    columns: columns
+    columns: columns,
+    config:config
   };
 
   useEffect(() => {
     tableData.name = name;
     tableData.url = url;
     tableData.sheetIndex = sheetIndex;
-  }, [name, url, sheetIndex]);
+    tableData.columns = columns;
+    tableData.config = config;
+  }, [name, url, sheetIndex, columns, config]);
 
   const handleLoad = async () => {
     if (tableData.name && tableData.url && tableData.sheetIndex) {
@@ -59,6 +65,40 @@ export default function Table({ tablelist, setTableList }) {
     //keys = Object.keys(test[0]);
     // alert(keys);
     setShowTable(true);
+  };
+
+  const handleInputChange = (event, key, field) => {
+    const { value } = event.target;
+    setConfig((prevConfig) => {
+      const updatedConfig = [...prevConfig];
+      const configIndex = updatedConfig.findIndex((item) => item.key === key);
+      if (configIndex !== -1) {
+        // If config already exists, update the field value
+        updatedConfig[configIndex][field] = value;
+      } else {
+        // If config does not exist, create a new config object
+        const newConfig = {
+          name: "",
+          key: "",
+          label: "",
+          reference: "",
+          type: ""
+        };
+        newConfig.name = key;
+        if (field === "label" || field === "key") {
+            newConfig[field] = value === "true";
+        } else {
+        newConfig[field] = value;
+        }
+        updatedConfig.push(newConfig);
+      }
+      return updatedConfig;
+    });
+  };
+
+  const handleCreateClick = () => {
+    // Use the config array to perform desired action with the configuration
+    console.log(config);
   };
 
   const handleAddTable = () => {
@@ -112,7 +152,73 @@ export default function Table({ tablelist, setTableList }) {
       <br />
       {showTable && (
         <div>
-         <ConfigTable columns = {columns}/>
+          <table className="table table-bordered">
+            <thead>
+                <tr>
+                <th>Name</th>
+                <th>Key</th>
+                <th>Label</th>
+                <th>Reference</th>
+                <th>Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                {keys.map((key) => (
+                <tr key={key}>
+                    <td>{key}</td>
+                    <td>
+                    <label>
+                        <input
+                        type="radio"
+                        name={`radio-col1`}
+                        value={key}
+                        onChange={(event) => handleInputChange(event, key, "key")}
+                        />
+                    </label>
+                    </td>
+                    <td>
+                    <label>
+                        <input
+                        type="radio"
+                        name={`radio-col2`}
+                        value={key}
+                        onChange={(event) => handleInputChange(event, key, "label")}
+                        />
+                    </label>
+                    </td>
+                    <td>
+                    <select
+                        name={`select-${key}`}
+                        onChange={(event) => handleInputChange(event, key, "reference")}
+                    >
+                        <option></option>
+                        {dummyRef.map((ref) => (
+                        <option key={ref} value={ref}>
+                            {ref}
+                        </option>
+                        ))}
+                    </select>
+                    </td>
+                    <td>
+                    <select
+                        name={`select-${key}`}
+                        onChange={(event) => handleInputChange(event, key, "type")}
+                    >
+                        <option></option>
+                        <option value="int">Number</option>
+                        <option value="bool">Boolean</option>
+                        <option value="string">Text</option>
+                        <option value="url">URL</option>
+                    </select>
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+            <div className="text-right">
+                <button onClick={handleCreateClick} className="btn btn-danger can_btn">Cancel</button>
+            <button onClick={handleCreateClick} className="btn btn-info">Create</button>
+            </div>
         </div>
       )}
 
