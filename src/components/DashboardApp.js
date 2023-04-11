70123dade46ea1c244203ba07ee16324e3eeea59
rmahanta1 <89950504+rmahanta1@users.noreply.github.com>
2023-04-11 11:31:52 -0400

    Fixing up Dashboard

--- src/components/DashboardApp.js
+++ src/components/DashboardApp.js
@@ -1,21 +1,20 @@
 import React from "react";
 
-const DashboardApp = ({name, date}) => {
-    return (
-        <div className="col-3" style={{flex: 1}}>
-              <a href="#">
-                <div className="card p-0 text-center">
-                  <h2 className="">{name}</h2>
-                  <hr />
-                  <div className="p-1">
-                    <small>Last modified {date}</small>
-                  </div>
-                </div>
-              </a>
-              <br/>
+const DashboardApp = ({ name, date }) => {
+  return (
+    <div className="col-3">
+      <a href="#">
+        <div className="card p-0 text-center">
+          <h2 className="">{name}</h2>
+          <hr />
+          <div className="p-1">
+            <small>Last modified {date}</small>
+          </div>
         </div>
-    );
-  };
-  
-  export default DashboardApp;
-  
+      </a>
+      <br />
+    </div>
+  );
+};
+
+export default DashboardApp;
