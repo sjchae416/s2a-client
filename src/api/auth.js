c2b78742be94f6394a37ce55c43c32801b739300
Seungjun Chae <seungjun.chae@stonybrook.edu>
2023-04-14 16:47:58 -0400

    implemented a fetchToken api wrapper

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,3 +3,22 @@ const defaultHeaders = {
 		'Content-Type': 'application/json',
 	},
 };
+
+export const fetchToken = async () => {
+	try {
+		const response = await fetch(`http://localhost:3333/auth/token`, {
+			method: 'GET',
+			headers: defaultHeaders,
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			const accessToken = data.accessToken;
+			return accessToken;
+		} else {
+			console.error('Failed to fetch access token');
+		}
+	} catch (error) {
+		console.error('Error fetching access token', error);
+	}
+};
