8a927c9d5b0fc3dad1a5bf859d074c587cb5c990
mlee990125 <michael.j.lee@stonybrook.edu>
2023-04-15 09:06:43 -0400

    implemented rest of tableApi.js

--- src/api/tableApi.js
+++ src/api/tableApi.js
@@ -10,10 +10,76 @@ export const loadTable = async (tableData) => {
 			headers: defaultHeader,
 			body: JSON.stringify(tableData),
 		});
-		const responseBody = await response.text(); // Read the response body as text
-		// console.log(responseBody ? responseBody : "no response");
+		const responseBody = await response.text(); 
 		const parsedData = JSON.parse(responseBody);
 		return parsedData;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const createTable = async (tableData) => {
+	try {
+		const response = await fetch("http://localhost:3333/tables/", {
+			method: "POST",
+			credentials: "include",
+			headers: defaultHeader,
+			body: JSON.stringify(tableData),
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const readAllTables = async () => {
+	try {
+		const response = await fetch("http://localhost:3333/tables/", {
+			method: "GET",
+			credentials: "include",
+			headers: defaultHeader,
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const readTable = async (id) => {
+	try {
+		const response = await fetch(`http://localhost:3333/tables/${id}`, {
+			method: "GET",
+			credentials: "include",
+			headers: defaultHeader,
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const updateTable = async (id, update) => {
+	try {
+		const response = await fetch(`http://localhost:3333/tables/${id}`, {
+			method: "PUT",
+			credentials: "include",
+			headers: defaultHeader,
+			body: JSON.stringify(update),
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const deleteTable = async (id) => {
+	try {
+		const response = await fetch(`http://localhost:3333/tables/${id}`, {
+			method: "DELETE",
+			credentials: "include",
+			headers: defaultHeader,
+		});
+		return response.status === 204;
 	} catch (error) {
 		console.error(error);
 	}
