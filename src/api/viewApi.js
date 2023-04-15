3556f2b334d0d47c6fcee40f370c58c38dea57b0
mlee990125 <michael.j.lee@stonybrook.edu>
2023-04-15 09:12:33 -0400

    implemented rest of viewApi.js

--- src/api/viewApi.js
+++ src/api/viewApi.js
@@ -1,3 +1,70 @@
 const defaultHeader = {
 	'Content-Type': 'application/json',
 };
+
+export const createView = async (viewData) => {
+	try {
+		const response = await fetch("http://localhost:3333/views/", {
+			method: "POST",
+			credentials: "include",
+			headers: defaultHeader,
+			body: JSON.stringify(viewData),
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const readAllViews = async () => {
+	try {
+		const response = await fetch("http://localhost:3333/views/", {
+			method: "GET",
+			credentials: "include",
+			headers: defaultHeader,
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const readView = async (id) => {
+	try {
+		const response = await fetch(`http://localhost:3333/views/${id}`, {
+			method: "GET",
+			credentials: "include",
+			headers: defaultHeader,
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const updateView = async (id, viewData) => {
+	try {
+		const response = await fetch(`http://localhost:3333/views/${id}`, {
+			method: "PUT",
+			credentials: "include",
+			headers: defaultHeader,
+			body: JSON.stringify(viewData),
+		});
+		return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export const deleteView = async (id) => {
+	try {
+		const response = await fetch(`http://localhost:3333/views/${id}`, {
+			method: "DELETE",
+			credentials: "include",
+			headers: defaultHeader,
+		});
+		return response.status === 204;
+	} catch (error) {
+		console.error(error);
+	}
+}
