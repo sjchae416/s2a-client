e7cf1d7e5c526a25389d00917cf376105beb1632
rmahanta1 <89950504+rmahanta1@users.noreply.github.com>
2023-04-24 16:17:21 -0400

    displaying list (checnges in both views nd tables)

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { actionSelectView, actionSelectTable } from "../redux/action";
 
-const List = ({ type }) => {
-  const { tableList, viewList } = useSelector((state) => state.app);
-  const dispatch = useDispatch();
+const List = ({ type, viewDataList, setSelectedView, setSelectedTable }) => {
+  const tables = JSON.parse(window.localStorage.getItem("tables"));
 
   return (
     <div>
       {type === "view"
-        ? viewList.map((item, ind) => (
-            <div key={ind} onClick={() => dispatch(actionSelectView(item))}>
-              <hr />
+        ? viewDataList.map((item) => (
+          <div key={item.id} onClick={() => setSelectedView(item)}>
+            <hr />
               {item.viewName}
             </div>
           ))
-        : tableList.map((item, ind) => (
-            <div key={ind} onClick={() => dispatch(actionSelectTable(item))}>
-              <hr />
-              {item.name}
-            </div>
-          ))}
+        : tables.map(
+            (item) => 
+              item && (
+                <div key={item._id} onClick={() => setSelectedTable(item)}>
+                  <hr />
+                  {item.name}
+                </div>
+              )
+          )}
     </div>
   );
 };
