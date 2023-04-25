4de2cdb7fcdfafa0d81598fd267173aaf29d6da6
rmahanta1 <89950504+rmahanta1@users.noreply.github.com>
2023-04-25 14:18:27 -0400

    removing redux components

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import appReducer from "./reducer";
-
-const rootReducer = combineReducers({
-  app: appReducer,
-});
-
-export default rootReducer;
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./rootReducer";
-
-const store = createStore(rootReducer);
-
-export default store;
-
-store.subscribe(() => console.log("Store updated", store.getState()));
