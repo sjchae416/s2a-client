af60e0f3b1005177e9b45a23872caa0012ba8a78
rmahanta1 <89950504+rmahanta1@users.noreply.github.com>
2023-04-27 16:32:41 -0400

    adding a component for rendering the correct table config

---
+++ src/components/SelectedTableConfig.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+// using this to print out the config of the selected tble
+const SelectedTableConfig = ({ keys, userTables }) => {
+  return (
+    <>
+      {keys.map((key) => (
+        <tr key={key._id}>
+          <td>{key.name}</td>
+          <td>
+            <label>
+              <input
+                type="radio"
+                name={`radio-col1`}
+                defaultChecked={key.key}
+              />
+            </label>
+          </td>
+          <td>
+            <label>
+              <input
+                type="radio"
+                name={`radio-col2`}
+                defaultChecked={key.label}
+              />
+            </label>
+          </td>
+          <td>
+            <select name={`select-${key}`} defaultValue="none">
+              <option value="none">None</option>
+              {userTables?.map((table) => (
+                <option key={table._id} value={table._id}>
+                  {table.name}
+                </option>
+              ))}
+            </select>
+          </td>
+          <td>
+            <select>
+              {[
+                { type: "string", name: "Text" },
+                { type: "int", name: "Number" },
+                { type: "bool", name: "Boolean" },
+                { type: "url", name: "URL" },
+              ].map((item) => (
+                <option
+                  key={item.type}
+                  selected={key.type === item.type ? true : false}
+                >
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
+
+export default SelectedTableConfig;
