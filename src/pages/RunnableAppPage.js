e1c30ee60976e33c5890eedd46bc83de0985e885
Seungjun Chae <seungjun.chae@stonybrook.edu>
2023-05-05 08:24:49 -0400

    updated lastOpenedDate and render it correctly now

--- src/pages/RunnableAppPage.js
+++ src/pages/RunnableAppPage.js
@@ -1,8 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { NavigationBar, TableView, TableViewSidebar } from '../components';
 import UserContext from '../UserContext';
 import { saveAs } from 'file-saver';
+import { updateAppAPI } from '../api';
 
 export default function RunnableAppPage({ runnableApps, userTables, appLog }) {
 	const { id } = useParams();
@@ -20,24 +21,43 @@ export default function RunnableAppPage({ runnableApps, userTables, appLog }) {
 	};
 
 	const handleDownloadLog = () => {
-		const appObj = appLog.find(obj => obj.app_id === id);
+		const appObj = appLog.find((obj) => obj.app_id === id);
 		const csv = convertObjectToCsv(appObj);
 
 		const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
-    	saveAs(blob, `${appObj.app_name}.csv`);
+		saveAs(blob, `${appObj.app_name}.csv`);
 	};
 
 	function convertObjectToCsv(obj) {
 		const headers = Object.keys(obj);
 		const values = headers.map((header) => {
-			if (header === "log") {
-			  return JSON.stringify(obj[header]);
+			if (header === 'log') {
+				return JSON.stringify(obj[header]);
 			}
 			return obj[header];
-		  });
+		});
 		const csvRows = [headers.join(','), values.join(',')];
 		return csvRows.join('\n');
 	}
+
+	useEffect(() => {
+		const updateLastOpenedDate = async () => {
+			const now = new Date();
+			const nycTimeString = now.toLocaleString('en-US', {
+				timeZone: 'America/New_York',
+			});
+			runnableApp.lastOpenedDate = nycTimeString;
+
+			const update = runnableApp;
+			try {
+				await updateAppAPI(id, update);
+			} catch (error) {
+				console.error('Failed update the last opened date: ', error);
+			}
+		};
+
+		updateLastOpenedDate();
+	}, []);
 
 	return (
 		<div>
@@ -48,7 +68,7 @@ export default function RunnableAppPage({ runnableApps, userTables, appLog }) {
 				<h2>{runnableApp.app.name}</h2>
 				<button onClick={handleDownloadLog}>Download Log</button>
 			</div>
-			
+
 			<div className="page-container">
 				<div className="sidebar-container">
 					<TableViewSidebar views={views} onSelectView={handleSelectView} />
@@ -60,8 +80,8 @@ export default function RunnableAppPage({ runnableApps, userTables, appLog }) {
 							listViews={views}
 							userTables={userTables}
 							user={user}
-							appLog = {appLog}
-							appId = {id}
+							appLog={appLog}
+							appId={id}
 						/>
 					)}
 				</div>
